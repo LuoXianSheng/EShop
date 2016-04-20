7e419ca27a4d2ea148e373703e470eeaa6db668c
Mr.luo <578293722@qq.com>
2016-04-20 22:16:27 +0800

    3

--- app/src/main/java/com/newer/eshop/MainActivity.java
+++ app/src/main/java/com/newer/eshop/MainActivity.java
@@ -2,6 +2,8 @@ package com.newer.eshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        GridView mGridView;
+//        ListView mListView;
+//        mListView.add
     }
 }
