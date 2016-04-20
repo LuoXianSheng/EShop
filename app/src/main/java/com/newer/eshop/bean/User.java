62272a0f29b9eded14508b7bb2b4c86b2ee2ab24
franzhou <654018207@qq.com>
2016-04-20 21:58:42 +0800

    Smple Change

--- app/src/main/java/com/newer/eshop/bean/User.java
+++ app/src/main/java/com/newer/eshop/bean/User.java
@@ -4,4 +4,23 @@ package com.newer.eshop.bean;
  * Created by Mr_LUO on 2016/4/20.
  */
 public class User {
+   //创建用户实体类
+    private  String name;
+    private  String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
